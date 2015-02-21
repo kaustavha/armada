f322d92f03457067c7c1c74ae722158a9dde4d9b
Lawrell <dj.lawrell@gmail.com>
2015-02-21 14:12:28 -0500

    Merge https://github.com/kaustavha/armada
    Conflicts:
    	index.html

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
  
+ 
+// This is a simple *viewmodel* - JavaScript that defines the data and behavior of your UI
+function AppViewModel() {
+    this.firstName = ko.observable("Bert");
+    this.lastName = ko.observable("Bertington");
+
+    this.fullName = ko.computed(function() {
+        return this.firstName() + " " + this.lastName();    
+    }, this);
+
+    this.capitalizeLastName = function() {
+        var currentVal = this.lastName();        // Read the current value
+        this.lastName(currentVal.toUpperCase()); // Write back a modified value
+    };    
+}
+
+// Activates knockout.js
+ko.applyBindings(new AppViewModel());
