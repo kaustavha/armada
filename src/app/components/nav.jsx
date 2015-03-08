7cffbc822dbc226fb424110f0735426c1682b2fc
kaustavha <hi@kaustav.me>
2015-03-08 06:09:18 -0400

    fix navbar and remove unused style files

--- src/app/components/nav.jsx
+++ src/app/components/nav.jsx
@@ -5,6 +5,7 @@ var Nav = React.createClass({
   render: function() {
     return (
       <div className="nav-button-container">
+        <img src='armada_logo.png' class='logo'></img>
         <RaisedButton linkButton={true} href="#/store" secondary={true} label='App Store' />
         <RaisedButton linkButton={true} href="#/myapps" secondary={true} label='My Apps' />
         <RaisedButton linkButton={true} href="#/developer" secondary={true} label='Developer Tools' />
